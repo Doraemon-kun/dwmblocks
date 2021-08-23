12c48769ffd8ebb431c72529f94b638acb13cba6
Doraemon-kun <doraemon-kun@protonmail.com>
2021-08-23 21:35:42 +0700

    v1.0

---
+++ blocks.h
@@ -0,0 +1,14 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/		/*Command*/							/*Update Interval*/	/*Update Signal*/
+	{"^c#F5CDA7^: ",	"~/.dwmscripts/dwmcpu", 					1,			0},
+	{"^c#EC9EAD^: ", 	"free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	1,			0},
+	{"^c#AABA78^", 		"~/.dwmscripts/dwmvolume",    					0,			13},
+	{"", 		"~/.dwmscripts/dwmbattery",					5,			0},
+	{"^c#FDC5F5^",		"~/.dwmscripts/dwminternet",					3,			0},
+	{"^c#bbbbbb^", 		"date '+%a %b %d %Y %H:%M'",					20,			0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = "    ";
+static unsigned int delimLen = 5;
